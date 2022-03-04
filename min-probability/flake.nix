6c49af445cf1f2d7778be3e6077394643c59de7f
Evangelos Parchas <parchas.e@gmail.com>
2022-03-04 10:31:37 +0200

    added initial nix flake

---
+++ min-probability/flake.nix
@@ -0,0 +1,19 @@
+{
+    description = 'Min probability code challenge',
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    },
+    outputs = { self, nixpkgs }:
+    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      devShell.x86_64-linux = pkgs.mkShell {
+        buildInputs =
+          [
+            pkgs.openjdk11
+            pkgs.maven
+            pkgs.nixpkgs-fmt
+          ];
+      };
+    };
+}
