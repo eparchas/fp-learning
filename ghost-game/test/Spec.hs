f434e02c235e08539659a4e764a918dd20123b54
Evangelos Parchas <parchas.e@gmail.com>
2022-03-28 22:44:47 +0300

    Added properties validation

--- ghost-game/test/Spec.hs
+++ ghost-game/test/Spec.hs
@@ -3,14 +3,28 @@ import qualified Data.ByteString.Char8 as BSU
 import Data.List (sort)
 import qualified Data.Set as S
 import qualified Data.Trie as T
-import Lib (calculateFirstLetters)
-import Test.Hspec (describe, hspec, it, shouldBe, Expectation)
+import Debug.Trace (trace)
+import Lib (calculateFirstLetters, calculateWinningSequences)
+import Test.Hspec ( hspec, describe, it, shouldBe, Expectation )
+import Test.QuickCheck
+    ( listOf,
+      listOf1,
+      forAll,
+      arbitraryASCIIChar,
+      Arbitrary(arbitrary),
+      Testable(property) )
 
-playsToWin :: [String] -> [String] -> Expectation 
+playsToWin :: [String] -> [String] -> Expectation
 playsToWin dict play = calculateFirstLetters dict `shouldBe` play
 
 main :: IO ()
 main = hspec $ do
+  describe "Check general properties" $ do
+    it "There should always be a winner for non empty dictionary" $
+      property $ forAll (listOf $ listOf1 arbitraryASCIIChar) $ \ws -> not $ null (calculateWinningSequences ws)
+    it "Order of input should not affect result" $
+      property $ forAll (listOf $ listOf1 arbitraryASCIIChar) $ \ws -> calculateWinningSequences ws == calculateWinningSequences (reverse ws)
+    
   describe "Check that first letters are caclulated correctly" $ do
     it "should return single winning word" $ do
       ["bee", "beer", "bear"] `playsToWin` ["b,a"]
@@ -20,12 +34,7 @@ main = hspec $ do
 
     it "should return empty list for secondary path" $ do
       ["bet", "betan", "better"] `playsToWin` []
-    
+
     it "should return equally winning words" $ do
-        ["bear", "calf"] `playsToWin` ["b", "c"]
-    
-{- |
-Properties
-- There should be a winner for every non zero length input
-- Order of input should not change who the winner is
--}
+      ["bear", "calf"] `playsToWin` ["b", "c"]
+
