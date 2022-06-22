03cf90fb79c5328fe5ba79547ef613587693c120
Evangelos Parchas <parchas.e@gmail.com>
2022-06-23 02:05:05 +0300

    Added tests

--- max-stock-profit/test/Spec.hs
+++ max-stock-profit/test/Spec.hs
@@ -1,2 +1,27 @@
+import Lib (maxProfit)
+import Test.Hspec (Expectation, describe, hspec, it, shouldBe)
+import Test.QuickCheck (Testable (property), getPositive, listOf, forAll, Positive (Positive), Arbitrary (arbitrary), arbitrarySizedNatural, NonEmptyList (NonEmpty))
+
+prop_increasedProfits :: Positive Int -> NonEmptyList (Positive Int) -> Bool
+prop_increasedProfits (Positive fee) (NonEmpty pps) = maxProfit fee pints <= maxProfit fee long where
+    pints = map getPositive pps
+    long = pints ++ [fee + 1]
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    describe "Check general properties" $ do
+      it "Should return increased profits for a sequence of prices and the same sequence plus a larger than cost end element" $
+        property prop_increasedProfits
+
+    describe "Check boundary conditions and valid price ranges" $ do
+      it "Should return no profit for empty price list" $ do
+        maxProfit 0 [] `shouldBe` 0
+      it "Should return no profit for constant price independently of fees" $ do
+        maxProfit 0 (replicate 20 1) `shouldBe` 0
+        maxProfit 1 (replicate 20 100) `shouldBe` 0
+      it "Should return no profit for declining price" $ do
+        maxProfit 0 [100, 99 .. 1] `shouldBe` 0
+      it "Should return no profit for ascending price where ascend does not exceed cost" $ do
+        maxProfit 10 [1 .. 10] `shouldBe` 0
+      it "Should return profit equal to the total ascend when there is no cost" $ do
+        maxProfit 0 [1 .. 10] `shouldBe` 9
